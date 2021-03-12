9d761e844dd8d4c2e3520895dd1abdee48e6b65f
Max Base <MaxBaseCode@Gmail.Com>
2021-03-12 10:57:53 +0330

    Create screenshot_all.sh

---
+++ .i3/screenshot_all.sh
@@ -0,0 +1,1 @@
+scrot --note "-f '/usr/local/share/fonts/TTF/Vazir-Light-FD/10' -x 10 -y 10 -c 0,0,0,255 -t 'Max Base'" -m '/tmp/%F_%T_$wx$h.png' -e 'xclip -selection clipboard -target image/png -i $f'
